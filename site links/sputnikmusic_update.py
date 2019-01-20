668bea9a5fd68df39e7dcead79459a13c0a6d603
Nick Proskurin <42863572+MCPN@users.noreply.github.com>
2019-01-20 13:43:04 +0300

    regex changed, dict() -> Counter(), utf8 -> iso-8859-1

--- site links/sputnikmusic_update.py
+++ site links/sputnikmusic_update.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from collections import Counter
+#from time import clock
 import re
 from urllib.request import urlopen
 import pywikibot
 from pywikibot import config
 from pywikibot import output
 
-REGEXP = r"https?://(?:www\.)?sputnikmusic\.com/(?:review/|album.php)[^ |\n]+"
+REGEXP = r"(https?://(?:www\.)?sputnikmusic\.com/(?:review/|album\.php).+?)/?[\s|}]"
 
 
 def check_user(link):
     try:
-        return "<font size=1 face=Arial class=brighttext>USER</font>" in urlopen(link).read().decode("utf-8")
+        return "<font size=1 face=Arial class=brighttext>USER</font>" in urlopen(link).read().decode("iso-8859-1")
     except:
         return False
     
@@ -20,7 +22,7 @@ def check_user(link):
 def check_contributor(link):
     try:
         return re.search(r"Review </h2>by <b>\n(?:.+?)<font size=1 face=Arial class=brighttext>CONTRIBUTOR</font>", 
-                          urlopen(link).read().decode("utf-8"))
+                          urlopen(link).read().decode("iso-8859-1"))
     except:
         return False
     
@@ -29,7 +31,7 @@ def main():
     site = pywikibot.Site()
     sputnik = pywikibot.Page(site, u"Участник:NPBot/Sputnikmusic Upgrade")
     whitelist = pywikibot.Page(site, u"Участник:NPBot/Sputnikmusic Upgrade/Whitelist")
-    goodPages = {link for link in whitelist.text.split()}
+    goodLinks = set(whitelist.text.split())
     badPagesCount = int(re.findall(r"Текущее количество: (\d+)", sputnik.text)[0])
     readPagesCount = 0
     done = 0
@@ -40,7 +42,7 @@ def main():
         title = re.findall(r"\[\[(.+?)\]\]", string)[0]
         page = pywikibot.Page(site, u"{}".format(title))
         links = [re.sub(r"http://", "https://", link) for link in re.findall(REGEXP, page.text, flags=re.I) 
-                 if re.sub(r"http://", "https://", link) not in goodPages and (check_user(link) or check_contributor(link))] 
+                 if re.sub(r"http://", "https://", link) not in goodLinks and (check_user(link) or check_contributor(link))]
         
         if not links:
             if readPagesCount == badPagesCount - 1:
@@ -48,18 +50,13 @@ def main():
             else:
                 sputnik.text = sputnik.text.replace("{}\n".format(string), "")
             done += 1
-        else:
-            dlink = dict()
-            for link in links:
-                if link[-1] == "/":
-                    link = link[:len(link) - 1]
-                dlink[link] = dlink.get(link, 0) + 1
-                
+        else:   
+            clink = Counter(links)
             new_string = "# [[{}]]: ".format(title)
-            for link in dlink.keys():
-                new_string += "[{}] ".format(link)
-                if dlink[link] > 1:
-                    new_string += "(x{}) ".format(dlink[link])
+            for link in clink.most_common():
+                new_string += "[{}] ".format(link[0])
+                if link[1] > 1:
+                    new_string += "(x{}) ".format(link[1])
             sputnik.text = sputnik.text.replace(string, new_string)
             
         readPagesCount += 1
